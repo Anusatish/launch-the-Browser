4a4b6d42cc8aaf87d683906391d532d27e84d02a
Anusa2018 <anudass2018@gmail.com>
2019-10-13 21:19:27 -0500

    Test1

---
+++ src/org/Dem/Test1.java
@@ -0,0 +1,15 @@
+package org.Dem;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\AnuSatishSaanvi\\eclipse-workspace\\Sample\\Driver\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.google.com/");
+			
+	}
+
+}
